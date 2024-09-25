1bdf163629a26c0eabac20ffd0dcd9132a7c4dcb
Logan <logan.gough26@d155.org>
2024-09-25 07:25:44 -0500

    Initial Commit

---
+++ ClickerAppGough/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickerAppGough
+//
+//  Created by LOGAN GOUGH on 9/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
